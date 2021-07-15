ca6af2222189286f9080cfb9e5db1fbd2214c498
Felipe Alfonso González <f.alfonso.go@gmail.com>
2021-07-15 01:21:37 -0400

    Update db.php

--- db.php
+++ db.php
@@ -21,14 +21,14 @@ mysql_select_db ("750") ;
 */
 // ---------------- NNP ------------------------
 
-$link1 = mysql_connect ("localhost", "felipe", "KWMEEGvaIpiyIrsd")
+$link1 = mysql_connect ("localhost", "felipe", "")
         or die ("No se puede conectar a MySQL ");
 
 mysql_select_db ("felipe") ;
 
 //
 
-$link2 = mysql_connect ("localhost", "felipe", "KWMEEGvaIpiyIrsd")
+$link2 = mysql_connect ("localhost", "felipe", "")
         or die ("No se puede conectar a MySQL ");
 
 mysql_select_db ("felipe") ;
