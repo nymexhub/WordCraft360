714f0d02a68d38450951379f1dd5e0d668602664
Felipe Alfonso González <f.alfonso.go@gmail.com>
2022-11-13 02:11:42 -0300

    fixes

--- db.php
+++ db.php
@@ -20,14 +20,16 @@ mysql_select_db ("750") ;
 */
 // ---------------- NNP ------------------------
 
-$link1 = mysql_connect ("localhost", "root", "")
+// $link1 = mysql_connect ("localhost", "root", "fafarafa")
+// mysqli_connect("localhost","root","")
+$link1 = mysql_connect("localhost", "root", "fafarafa")
         or die ("No se puede conectar a MySQL ");
 
 mysql_select_db ("750W") ;
 
 //
 
-$link2 = mysql_connect ("localhost", "root", "")
+$link2 = mysql_connect("localhost", "root", "fafarafa")
         or die ("No se puede conectar a MySQL ");
 
 mysql_select_db ("750W") ;
--- db_nnp/db.php
+++ db_nnp/db.php
@@ -21,15 +21,16 @@ mysql_select_db ("750") ;
 // ---------------- NNP ------------------------
 
 $link1 = mysql_connect ("localhost", "felipe", "")
+$link1 = mysql_connect ("localhost", "root", "fafarafa")
         or die ("No se puede conectar a MySQL ");
 
-mysql_select_db ("felipe") ;
+mysql_select_db ("750W") ;
 
 //
 
-$link2 = mysql_connect ("localhost", "felipe", "")
+$link2 = mysql_connect ("localhost", "root", "fafarafa")
         or die ("No se puede conectar a MySQL ");
 
-mysql_select_db ("felipe") ;
+mysql_select_db ("750W") ;
 
 ?>
