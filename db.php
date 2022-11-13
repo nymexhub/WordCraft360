d959ef7315f4e8c9f96df459be9730ae1a276024
Felipe Alfonso González <f.alfonso.go@gmail.com>
2022-11-13 02:15:05 -0300

    ..

--- db.php
+++ db.php
@@ -6,30 +6,30 @@ Login: felipe
 */
 
 /* -------------- local host -----------------
-$link1 = mysql_connect ("localhost", "root", "fafarafa")
+$link1 = mysql_connect ("localhost", "root", "")
         or die ("No se puede conectar a MySQL ");
 
 mysql_select_db ("750") ;
 
 //
 
-	$link2 = mysql_connect ("localhost", "root", "fafarafa")
+	$link2 = mysql_connect ("localhost", "root", "")
         or die ("No se puede conectar a MySQL ");
 
 mysql_select_db ("750") ;
 */
 // ---------------- NNP ------------------------
 
-// $link1 = mysql_connect ("localhost", "root", "fafarafa")
+// $link1 = mysql_connect ("localhost", "root", "")
 // mysqli_connect("localhost","root","")
-$link1 = mysql_connect("localhost", "root", "fafarafa")
+$link1 = mysql_connect("localhost", "root", "")
         or die ("No se puede conectar a MySQL ");
 
 mysql_select_db ("750W") ;
 
 //
 
-$link2 = mysql_connect("localhost", "root", "fafarafa")
+$link2 = mysql_connect("localhost", "root", "")
         or die ("No se puede conectar a MySQL ");
 
 mysql_select_db ("750W") ;
--- db_nnp/db.php
+++ db_nnp/db.php
@@ -6,14 +6,14 @@ Login: felipe
 */
 
 /* -------------- local host -----------------
-$link1 = mysql_connect ("localhost", "root", "fafarafa")
+$link1 = mysql_connect ("localhost", "root", "")
         or die ("No se puede conectar a MySQL ");
 
 mysql_select_db ("750") ;
 
 //
 
-	$link2 = mysql_connect ("localhost", "root", "fafarafa")
+	$link2 = mysql_connect ("localhost", "root", "")
         or die ("No se puede conectar a MySQL ");
 
 mysql_select_db ("750") ;
@@ -21,14 +21,14 @@ mysql_select_db ("750") ;
 // ---------------- NNP ------------------------
 
 $link1 = mysql_connect ("localhost", "felipe", "")
-$link1 = mysql_connect ("localhost", "root", "fafarafa")
+//$link3 = mysql_connect ("localhost", "root", "")
         or die ("No se puede conectar a MySQL ");
 
 mysql_select_db ("750W") ;
 
 //
 
-$link2 = mysql_connect ("localhost", "root", "fafarafa")
+$link2 = mysql_connect ("localhost", "root", "")
         or die ("No se puede conectar a MySQL ");
 
 mysql_select_db ("750W") ;
